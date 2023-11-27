ee414ed07d8a556782b0e9a15bffb3026fc66efa
Denis <oktilon@gmail.com>
2023-11-27 23:00:49 +0200

    mixer update

--- src/mixer.c
+++ src/mixer.c
@@ -3,18 +3,33 @@
 #include "app.h"
 #include "mixer.h"
 
-static int sset_enum (snd_mixer_elem_t *elem, uint8_t vol);
-static int sset_channels (snd_mixer_elem_t *elem, uint8_t vol);
-static int set_volume_simple (snd_mixer_elem_t *elem, snd_mixer_selem_channel_id_t chn, uint8_t vol);
-
+static int mixer_process_volume (uint8_t *vol, int readValue);
+static void mixer_set_playback_volume (snd_mixer_elem_t *elem, long val);
+static long mixer_get_playback_volume (snd_mixer_elem_t *elem);
+static long convert_to_raw (snd_mixer_elem_t *elem, uint8_t vol);
+static uint8_t convert_to_percent (snd_mixer_elem_t *elem, long val);
 
 static char card[64] = "default";
 
 int mixer_set_volume (uint8_t vol) {
+    return mixer_process_volume (&vol, FALSE);
+}
+
+uint8_t mixer_get_volume () {
+    uint8_t vol = 0;
+
+    mixer_process_volume (&vol, TRUE);
+
+    return vol;
+}
+
+static int mixer_process_volume (uint8_t *vol, int readValue) {
     int err = 0;
     snd_mixer_t *handle = NULL;
     snd_mixer_elem_t *elem;
     snd_mixer_selem_id_t *sid;
+    long val = 0;
+
 
     snd_mixer_selem_id_alloca (&sid);
     snd_mixer_selem_id_set_index (sid, 0);
@@ -47,76 +62,103 @@ int mixer_set_volume (uint8_t vol) {
         return -ENOENT;
     }
 
-    /* enum control */
-    if (snd_mixer_selem_is_enumerated(elem))
-        err = sset_enum (elem, vol);
-    else
-        err = sset_channels (elem, vol);
+    err = snd_mixer_selem_has_playback_volume (elem);
+    if (err <= 0) {
+        selfLogWrn ("Simple control doesn't have Playback volume");
+        snd_mixer_close(handle);
+        return -EINVAL;
+    }
 
-    if (err < 0) {
-        selfLogErr ("Invalid command!"); // ?????
-    } else if (err) {
-        selfLogInf ("Simple mixer control '%s',%i\n", snd_mixer_selem_id_get_name(sid), snd_mixer_selem_id_get_index(sid));
-        // show_selem(handle, sid, "  ", 1);
+    if (readValue) {
+        val = mixer_get_playback_volume (elem);
+        *vol = convert_to_percent (elem, val);
+    } else {
+        val = convert_to_raw (elem, *vol);
+        if (val < 0)
+            return -EPERM;
+        mixer_set_playback_volume (elem, val);
     }
 
     snd_mixer_close (handle);
     return 0;
 }
 
-uint8_t mixer_get_volume () {
-    return 32;
-}
+static void mixer_set_playback_volume (snd_mixer_elem_t *elem, long val) {
+    int err;
+    snd_mixer_selem_channel_id_t chn;
 
-static int sset_enum(snd_mixer_elem_t *elem, uint8_t vol) {
-    selfLogInf ("sset_enum");
-    return 0;
-}
-
-static int sset_channels(snd_mixer_elem_t *elem, uint8_t vol) {
-    selfLogInf ("sset_channels");
-	unsigned int channels = ~0U; // All channels
-	unsigned int dir = 3; // 1-playback, 2-capture
-	unsigned int okflag = 3;
-	unsigned int idx;
-	snd_mixer_selem_channel_id_t chn;
-	int check_flag = -1;
-    int multi = 0;
-
-    for (chn = 0; chn <= SND_MIXER_SCHN_LAST; chn++) {
-        char *sptr = NULL;
-        int ival;
-
-        // Playback
+    for (chn = 0; chn <= SND_MIXER_SCHN_REAR_CENTER; chn++) {
         if (snd_mixer_selem_has_playback_channel (elem, chn)) {
-            if (set_volume_simple(elem, chn, vol) >= 0)
-                check_flag = 1;
+            err = snd_mixer_selem_set_playback_volume (elem, chn, val);
+            if (err < 0) {
+                selfLogDbg ("Set channel:%d volume error(%d): %s", chn, err, snd_strerror (err));
+                continue;
+            } else
+                selfLogTrc ("Channel:%d volume=%d", chn, val);
         }
+    }
+}
 
+static long mixer_get_playback_volume (snd_mixer_elem_t *elem) {
+    int err;
+    long val = 0;
+    snd_mixer_selem_channel_id_t chn;
+
+    for (chn = 0; chn <= SND_MIXER_SCHN_REAR_CENTER; chn++) {
+        if (snd_mixer_selem_has_playback_channel (elem, chn)) {
+            err = snd_mixer_selem_get_playback_volume (elem, chn, &val);
+            if (err < 0) {
+                selfLogDbg ("Get channel:%d volume error(%d): %s", chn, err, snd_strerror (err));
+                continue;
+            } else {
+                selfLogTrc ("Channel:%d volume=%d", chn, val);
+                break;
+            }
+        }
     }
 
-    return 0;
+    return val;
 }
 
-static int set_volume_simple(snd_mixer_elem_t *elem, snd_mixer_selem_channel_id_t chn, uint8_t vol) {
+static long convert_to_raw (snd_mixer_elem_t *elem, uint8_t vol) {
     int err;
     long val, pMin, pMax, tmp;
 
-    err = snd_mixer_selem_has_playback_volume (elem);
-    selfLogDbg ("snd_mixer_selem_has_playback_volume return %d", err);
-
-    // get_range = snd_mixer_selem_get_playback_volume_range
-    // get = snd_mixer_selem_get_playback_volume
-    // set = snd_mixer_selem_set_playback_volume
-
     err = snd_mixer_selem_get_playback_volume_range (elem, &pMin, &pMax);
+    if (err < 0) {
+        selfLogErr ("Get volume range error(%d): %s", err, snd_strerror (err));
+        return -EPERM;
+    }
 
     // Convert percent to value within range
     tmp = rint ((double)vol * (double)(pMax - pMin) * 0.01);
     if (tmp == 0 && vol > 0)
         tmp++;
     val = tmp + pMin;
+    // Check over range
+    val = val < pMin ? pMin : (val > pMax ? pMax : val);
+    selfLogDbg ("Percent [%d] to val=%d", vol, val);
 
-    //
-    err = snd_mixer_selem_set_playback_volume (elem, chn, val);
+    return val;
+}
+
+static uint8_t convert_to_percent (snd_mixer_elem_t *elem, long val) {
+    int err;
+    uint8_t vol = 0;
+    long pMin, pMax, dVal;
+
+    err = snd_mixer_selem_get_playback_volume_range (elem, &pMin, &pMax);
+    if (err < 0) {
+        selfLogErr ("Get volume range error(%d): %s", err, snd_strerror (err));
+        return -EPERM;
+    }
+
+    // Convert percent to value within range
+    dVal = pMax - pMin;
+    if (dVal) {
+        vol = rint (100.0 * ((double)val - (double)pMin) / (double)dVal);
+    }
+    selfLogDbg ("Val [%d] to percent %d%%", val, vol);
+
+    return vol;
 }
